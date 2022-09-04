b98407fa0baca052b816835623e242165330166c
Nikhil Prakash <nikhilprakash510@gmail.com>
2022-09-04 14:39:48 +0530

    Added GenresController

---
+++ src/Movies.API/Controllers/GenresController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movies.Domain.Entities;
+using Movies.Infrastructure;
+
+namespace Movies.API.Controllers
+{
+    [ApiController]
+    [Route("Genres")]
+    public class GenresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public GenresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Genre>> GetAllGenre()
+        {
+            return await context.Genres.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetGenreById(int id)
+        {
+            Genre genres = await context.Genres.FirstOrDefaultAsync(g => g.Id == id);
+
+            if (genres is null)
+            {
+                return NotFound($"The genre with id {id} does not exist!");
+            }
+            return Ok(genres);
+        }
+    }
+}
---
+++ src/Movies.API/Controllers/MoviesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.API.Controllers
+{
+    [ApiController]
+    [Route("Movies")]
+    public class MoviesController : ControllerBase
+    {
+    }
+}
