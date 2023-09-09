7c11bacd002e6fe95da18c8e3c08e132a4dd4cbf
Nikhil Prakash <nikhilprakash510@gmail.com>
2023-09-09 16:44:24 +0530

    Fixed Timestamp mismatch error during update movies

--- src/Movies.API/Program.cs
+++ src/Movies.API/Program.cs
@@ -1,4 +1,4 @@
-using FluentValidation.AspNetCore;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Movies.API;
 using Movies.API.Middlewares;
@@ -6,17 +6,20 @@ using Movies.API.Repositories;
 using Movies.API.Repositories.Interfaces;
 using Movies.API.Services;
 using Movies.API.Services.Interfaces;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>(ServiceLifetime.Transient);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddFluentValidation(x => x.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
+//builder.Services.AddFluentValidation(x => x.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer("name=DefaultConnection");
