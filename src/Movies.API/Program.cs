6a69dc8c8a6a91de8fe1ade7f7865180d04a9f8c
Nikhil Prakash <nikhilprakash510@gmail.com>
2023-10-22 17:23:10 +0530

    Enabled swagger for production

--- src/Movies.API/Program.cs
+++ src/Movies.API/Program.cs
@@ -35,11 +35,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseMiddleware<UnhandledExceptionMiddleware>();
 app.UseHttpsRedirection();
 app.UseAuthorization();
